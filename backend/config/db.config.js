8d93d2c86410c3abbc878042b477eda1cec884b5
peterkahumu <muhumukip@gmail.com>
2024-08-15 11:00:27 +0300

    Added  LICENSE and use Instructions.

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,9 +1,10 @@
 const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize("my_expenses", 'root', '1234' , {
     host: 'localhost',
-    port: 3300,
+    port: 3300, // default port for mysql is 3306
     dialect: 'mysql',
     dialectModule: require('mysql2'), // use mysql2 module.
+    logging: false
 });
 
 module.exports = sequelize;
